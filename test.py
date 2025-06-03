4c3224f5b150c4d5b6d57cf72d41e9f30396de1a
Peter Kim <Peter.Kim@us.yazaki.com>
2025-06-03 10:38:04 -0400

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+file_path = r"C:\Users\10817992\Downloads\testing_python.xlsx"
+
+# Read first 9 rows, no header
+data = pd.read_excel(file_path, sheet_name="Sheet1", header=None, engine="openpyxl")
+
+# print(data.to_string(index=False))
+
+csv_output_path = r"C:\Users\10817992\repos\excel_import\output.csv"
+data.to_csv(csv_output_path, index=False)
+
+print(f"CSV saved to {csv_output_path}")
